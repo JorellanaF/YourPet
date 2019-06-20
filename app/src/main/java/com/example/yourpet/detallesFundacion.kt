d533d9ccbf574e8593db8693f0aea15e7dfd5995
JorellanaF <00103717@uca.edu.sv>
2019-06-20 09:12:21 -0600

    nuevo Kevin

---
+++ app/src/main/java/com/example/yourpet/detallesFundacion.kt
@@ -0,0 +1,68 @@
+package com.example.yourpet
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_detalles_fundacion.*
+import kotlinx.android.synthetic.main.fragment_details_fundacion.*
+import kotlinx.android.synthetic.main.fragment_details_fundacion.tv_detail_contacto
+import kotlinx.android.synthetic.main.fragment_details_fundacion.tv_detail_descripcion
+import kotlinx.android.synthetic.main.fragment_details_fundacion.tv_detail_nombre
+
+class detallesFundacion : AppCompatActivity() {
+
+    var context: Context = this
+    var detalleNombre: TextView? = null
+    var detalleDescripcion: TextView? = null
+    var detalleContacto: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles_fundacion)
+
+        detalleNombre = findViewById(R.id.tv_detalle_nombre)
+        detalleDescripcion = findViewById(R.id.tv_detalle_descripcion)
+        detalleContacto = findViewById(R.id.tv_detail_contacto)
+
+    }
+
+    fun relleno(fund: String){
+        Log.d("recibido", "Si recibi siiiiiii" + fund)
+        var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        var myRef: DatabaseReference = database.getReference("fundaciones").child(fund)
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                var imagen: String = p0.child("imagen").getValue().toString()
+                Log.d("datos", "0 " + imagen)
+                /*Glide.with(context.applicationContext)
+                    .load(p0.child("imagen").getValue().toString())
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(iv_detalle_logo)*/
+                var nombre: String = p0.child("nombre_fundacion").getValue().toString()
+                Log.d("datos", "1 " + nombre)
+                context.apply {
+                    detalleNombre?.text = nombre
+                }
+                var descripcion: String = p0.child("descripcion").getValue().toString()
+                Log.d("datos", "2 " + descripcion)
+                detalleDescripcion?.text = descripcion
+                var contacto: String = p0.child("contacto").getValue().toString()
+                Log.d("datos", "3 " + contacto)
+                detalleContacto?.text = contacto
+            }
+
+        })
+    }
+
+}
